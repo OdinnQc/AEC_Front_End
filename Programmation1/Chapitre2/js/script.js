a9898558ae39c9c6ba3a6bc1f8833f614569a028
OdinnQc <lucas@odinnqc.biz>
2019-11-21 20:19:18 -0500

    Je Me Présente v2 avec CSS

--- Programmation1/Chapitre2/js/script.js
+++ Programmation1/Chapitre2/js/script.js
@@ -1,5 +1,19 @@
-/** créé pas lucas **/
+/* cree pas Lucas Comtois */
 
+//trouver les variables
 var nb1;
 var nb2;
 
+//demandez le nomdre de gallons
+nb1 = Number(prompt("Nombre 1"));
+nb2 = Number(prompt("Nombre 2"));
+
+//on fait le calcule
+if (nb1 > nb2){
+    //on affiche le résultat
+    alert("le nombre " + nb1 + " est plus grand que " + nb2);
+}
+else{
+    //on affiche le résultat
+    alert("le nombre " + nb2 + " est plus grand que " + nb1);
+}
